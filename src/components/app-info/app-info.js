946d8fe3cdc76bfe8da834dab007daf27de3ec8d
Maksym <maksymRozhko7@gmail.com>
2022-03-21 01:32:02 +0200

    create new app, create components

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,19 @@
+import './app-info.css';
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1 className="app-info__title">
+                Учет сотрудников в компании RDevMR
+            </h1>
+            <h2 className="app-info__subtitle">
+                Общее число сотрудников:
+            </h2>
+            <h2 className="app-info__subtitle">
+                Премию получат:
+            </h2>
+        </div>
+    );
+};
+
+export default AppInfo;
