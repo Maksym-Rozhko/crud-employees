767566d77db010b685aed11f9ef1e2f18c94da63
Maksym <maksymRozhko7@gmail.com>
2022-03-21 02:11:46 +0200

    add props

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,11 +1,45 @@
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
-import './app.css';
 import EmployeesList from '../employees-list/employees-list';
 import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
+import './app.css';
+
 function App() {
+    const data = [
+        {
+            name: 'John',
+            surname: 'Smith',
+            salary: 500,
+            increase: true,
+        },
+        {
+            name: 'Alex',
+            surname: 'Mask',
+            salary: 1000,
+            increase: false,
+        },
+        {
+            name: 'George',
+            surname: 'Shepard',
+            salary: 1500,
+            increase: true,
+        },
+        {
+            name: 'Elona',
+            surname: 'Sagerse',
+            salary: 1750,
+            increase: true,
+        },
+        {
+            name: 'Ignat',
+            surname: 'Callel',
+            salary: 750,
+            increase: false,
+        },
+    ];
+
     return (
         <div className="app">
             <AppInfo />
@@ -14,7 +48,7 @@ function App() {
                 <SearchPanel />
                 <AppFilter />
             </div>
-            <EmployeesList />
+            <EmployeesList data={data} />
             <EmployeesAddForm />
         </div>
     );
